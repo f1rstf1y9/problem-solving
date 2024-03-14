79ff73bb7ac132f62c70ab029070052590e1aee8
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2024-03-15 00:06:47 +0900

    Time: 51 ms (93.09%) | Memory: 17.6 MB (60.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = list(sorted([(i,nums[i]) for i in range(len(nums))],key=lambda x:x[1]))
-        l, r = 0, len(nums)-1
-        while l < r:
-            if nums_dict[l][1]+nums_dict[r][1] > target:
-                r -= 1
-            elif nums_dict[l][1]+nums_dict[r][1] < target:
-                l += 1
+        checked_nums = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in checked_nums:
+                return [checked_nums[diff], i]
             else:
-                return list(sorted([nums_dict[l][0],nums_dict[r][0]]))
+                checked_nums[nums[i]] = i
