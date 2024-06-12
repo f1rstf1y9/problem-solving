4876c18b7e67fafa5bc6545712c379f1d9d53309
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2024-06-12 10:47:57 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,3 +1,19 @@
+function bfs(queue, computers, visited, n) {
+    while (queue.length) {
+        const currentComputer = queue.pop();
+        const connectedComputers = computers[currentComputer];
+        visited[currentComputer] = true;
+        for (let connectedComputer = 0; connectedComputer < n; connectedComputer++) {
+            if (connectedComputer === currentComputer || !connectedComputers[connectedComputer]) {
+                continue;
+            }
+            if (!visited[connectedComputer]) {
+                queue.push(connectedComputer);
+            }
+        }
+    }
+}
+
 function solution(n, computers) {
     var answer = 0;
     let visited = new Array(n).fill(false);
@@ -7,21 +23,7 @@ function solution(n, computers) {
         if (!visited[computer]) {
             answer++;
             queue.push(computer);
-            
-            while (queue.length) {
-                const currentComputer = queue.pop();
-                const connectedComputers = computers[currentComputer];
-                visited[currentComputer] = true;
-                for (let connectedComputer = 0; connectedComputer < n; connectedComputer++) {
-                    if (connectedComputer === currentComputer || !connectedComputers[connectedComputer]) {
-                        continue;
-                    }
-                    if (!visited[connectedComputer]) {
-                        queue.push(connectedComputer);
-                    }
-                }
-            }
-            
+            bfs(queue, computers, visited, n);
         }
     }
     
