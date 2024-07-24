d0ddd1557548049d11210dacf6a077fb319aabf2
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2024-07-24 23:57:43 +0900

    [Bronze II] Title: 사각형 면적, Time: 448 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22341. 사각형 면적/사각형 면적.py
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.py
@@ -2,13 +2,11 @@ N, C = map(int, input().split())
 width = height = N
 for _ in range(C):
     x, y = map(int, input().split())
-    if x < width and y < height:
-        if x * height > width * y:
-            width = x
-        else:
-            height = y
-    elif x < width:
-        width = x
-    elif y < height:
-        height = y
+    if x >= height or y >= width:
+        continue;
+    if x * width >= y * height:
+        height = x
+    else:
+        width = y
+
 print(width*height)
