f7ee5f824826eee10bb6cf9abb77aef4f1c6f667
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2024-10-19 18:14:56 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 72 ms, Memory: 44160 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -6,22 +6,7 @@ positions = list(map(int, input().split()))
 
 distances = [positions[0]]
 for i in range(M-1):
-  distances.append(math.ceil((positions[i+1]-positions[i])/2))
+  distances.append(math.ceil((positions[i+1] - positions[i])/2))
 distances.append(N-positions[M-1])
 
-low = 1
-high = N
-
-def can_iluminate(H):
-  for distance in distances:
-    if distance > H:
-      return False
-  return True
-
-while low <= high:
-  mid = (low+high)//2  
-  if can_iluminate(mid):
-    high = mid - 1
-  else:
-    low = mid + 1
-print(low)
+print(max(distances))
