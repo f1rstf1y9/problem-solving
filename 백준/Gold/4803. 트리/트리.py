427b0a02593949517c95dd1204e5c89cc88fb6c7
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2024-12-17 23:56:08 +0900

    [Gold IV] Title: 트리, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -16,24 +16,22 @@ def union(a, b, parents):
 
 def count_trees(n, m):
     parents = [i for i in range(n+1)]
-    cycle = set()
+    is_tree = [True]*(n+1)
     
     for _ in range(m):
-        e1, e2 = map(int, input().split())
-        if find(e1, parents) == find(e2, parents):
-            cycle.add(parents[e1])
-            cycle.add(parents[e2])
-        elif parents[e1] in cycle or parents[e2] in cycle:
-            cycle.add(parents[e1])
-            cycle.add(parents[e2])
-        union(e1, e2, parents)
+      v1, v2 = map(int, input().split())
+      r1 = find(v1, parents)
+      r2 = find(v2, parents)
+      if r1 == r2:
+          is_tree[r1] = False
+      else:
+          if not is_tree[r1] or not is_tree[r2]:
+              is_tree[r1] = is_tree[r2] = False
+          union(v1, v2, parents)
     
-    for i in range(n+1):
-        find(i, parents)
-
     trees_cnt = 0
     for i in range(1, n+1):
-        if parents[i] == i and i not in cycle:
+        if parents[i] == i and is_tree[i]:
             trees_cnt += 1
     
     return trees_cnt
@@ -43,9 +41,8 @@ while True:
     n, m = map(int, input().split())
     if n == 0 and m == 0:
         break
-    
+        
     trees_cnt = count_trees(n, m)
-
     if trees_cnt == 0:
         print(f"Case {tc}: No trees.")
     elif trees_cnt == 1:
