5841c9be014e0aecce2f6265e6489b71292b15fc
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2025-03-20 00:19:36 +0900

    [Gold V] Title: 암호코드, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -9,12 +9,13 @@ if pwd[0] == '0':
     
 dp = [1] + [0]*(N-1)
 
-if N > 1:
-    dp[1] = (1 if 1 <= int(pwd[1]) <= 9 else 0) + (1 if 10 <= int(pwd[0]+pwd[1]) <= 26 else 0)
-
-for i in range(2, N):
+for i in range(1, N):
     p1 = 1 if 1 <= int(pwd[i]) <= 9 else 0
     p2 = 1 if 10 <= int(pwd[i-1]+pwd[i]) <= 26 else 0
-    dp[i] = (dp[i-1]*p1 + dp[i-2]*p2) % 1000000
+    
+    if i == 1:
+        dp[i] = p1 + p2
+    else:
+        dp[i] = (dp[i-1]*p1 + dp[i-2]*p2) % 1000000
 
-print(dp[len(pwd)-1])
+print(dp[N-1])
