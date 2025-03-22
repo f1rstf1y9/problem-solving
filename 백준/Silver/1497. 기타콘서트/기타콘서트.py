0bb9be8e84ff7d8456be90985e4c5833d6d46521
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2025-03-22 22:46:26 +0900

    [Silver I] Title: 기타콘서트, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+max_value = max_count = 0
+
+def backtracking(current_value, current_idx, current_count):
+    global max_value, max_count
+    
+    if current_value == max_value:
+        max_count = min(max_count, current_count)
+    if current_value > max_value:
+        max_value, max_count = current_value, current_count
+        
+    if  max_count == 2**M or current_idx == N:
+        return
+        
+    backtracking(current_value | guitars[current_idx], current_idx+1, current_count+1)
+    backtracking(current_value, current_idx+1, current_count)
+    
+
+N, M = map(int, input().split())
+
+guitars = [int(input().split()[1].replace('Y', '1').replace('N', '0'), 2) for _ in range(N)]
+
+backtracking(0, -1, 0)
+
+if max_value == 0:
+    print(-1)
+else:
+    print(max_count)
