c988e623886789286493277d35c39489e0991872
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2025-03-27 17:02:38 +0900

    [Silver I] Title: 카잉 달력, Time: 3920 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,22 +1,24 @@
+from math import gcd
 import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     M, N, x, y = map(int, input().split())
     
-    count = 1
+    max_count = M*N//gcd(M,N)
+    
+    count = x
     a = x
 
-    while count <= N:
+    while count <= max_count:
         if a > N:
             a = a%N if a%N else N
             
         if a == y:
-            print(M*(count-1)+x)
+            print(count)
             break
             
         a += M
-        count += 1
+        count += M
     else:
         print(-1)
-        
