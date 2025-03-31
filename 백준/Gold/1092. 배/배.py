4933e66fb074b76d3f4f69feb2766335243317a8
정은경 <94157035+f1rstf1y9@users.noreply.github.com>
2025-03-31 11:34:16 +0900

    [Gold V] Title: 배, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,29 +1,22 @@
 import sys
-from bisect import bisect_right
-
 input = sys.stdin.readline
 
 N = int(input())
 weight_limits = list(sorted(map(int, input().split()), reverse=True))
 M = int(input())
-weights = list(sorted(map(int, input().split())))
+weights = list(sorted(map(int, input().split()), reverse=True))
 
-prev_count = count = 0
-time = 1
+if weight_limits[0] < weights[0]:
+    print(-1)
+    exit()
 
-while count < M:
-    for weight_limit in weight_limits:
-        idx = bisect_right(weights,weight_limit)-1
-        if 0 > idx: continue
-        
-        del weights[idx]
-        count += 1
-        
-    if prev_count == count:
-        print(-1)
-        break
-    prev_count = count
-    time += 1
-else:
-    print(time-1)
-        
+working_time = [0]*N
+for weight in weights:
+    best_crane_idx = 0
+    for crane_idx, weight_limit in enumerate(weight_limits):
+        if weight_limit < weight:
+            break
+        if working_time[crane_idx] < working_time[best_crane_idx]:
+            best_crane_idx = crane_idx
+    working_time[best_crane_idx] += 1
+print(max(working_time))
